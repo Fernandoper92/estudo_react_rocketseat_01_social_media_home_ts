ba4511733523ae1183e1a8f12ca7e095db70d447
Fernando Pereira <fernandoper92@gmail.com>
2023-08-30 00:24:12 -0300

    subindo projeto

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post, PostType } from "./components/Post";
+import "./global.css";
+import styles from "./App.module.css";
+
+//author: {avatar_url"", name: "", role: ""}
+//publishedAt: Date
+// content: String
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl:
+        "https://images.unsplash.com/photo-1520423465871-0866049020b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=30",
+      name: "Franciele da Silva",
+      role: "Designer",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2023-08-20 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl:
+        "https://images.unsplash.com/photo-1520423465871-0866049020b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=30",
+      name: "Franciele da Silva",
+      role: "Designer",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2023-08-25 20:00:00"),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                post={post}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
